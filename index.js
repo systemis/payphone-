ed2f8332e8984c6ee327a0ce03a70093b5919862
systemis <cuthinh2002@icloud.com>
2019-01-19 18:32:24 +0700

    V2- done game

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ app.get('/', (req, res) => {
 const server = require('http').Server(app);
 server.listen(3000, () => {
   console.log('Server listening');
+  // console.log();
 })
--- public/game.js
+++ public/game.js
@@ -17,7 +17,8 @@ var config = {
 }
 
 var game = new Phaser.Game(config);
-var platforms, player, stars, cursors;
+var platforms, player, stars, cursors, bombs = [];
+var score = 0, scoreText;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -34,11 +35,16 @@ function create() {
 
   cursors = this.input.keyboard.createCursorKeys();
   platforms = this.physics.add.staticGroup();
+  bombs = this.physics.add.staticGroup();
+
+  scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
   platforms.create(600, 400, 'ground').refreshBody();
   platforms.create(50, 250, 'ground').refreshBody();
   platforms.create(750, 220, 'ground').refreshBody();
+
+
 
   player = this.physics.add.sprite(100, 150, 'dude');
   player.setBounce(0.2);
@@ -68,9 +74,21 @@ function create() {
 
   stars = this.physics.add.group({
     key: 'star',
-    repeat: 0,
+    repeat: 10,
     setXY: { x: 32, y: 0, stepX: 70 }
   })
+
+  // bombs = this.physics.add.group({
+  //   key: 'bomb',
+  //   repeat: 5,
+  //   setXY: { x: 30, y: 8, stepX: 10 }
+  // })
+
+  for (let i = 0; i < 7; i++) {
+    let xPos = Math.floor((Math.random() * 600) + 1);
+    let yPos = Math.floor((Math.random() * 600) + 1);
+    bombs.create(xPos, yPos, 'bomb').refreshBody();
+  }
 
   stars.children.iterate(child => {
     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
@@ -78,16 +96,27 @@ function create() {
 
 
   this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
   this.physics.add.collider(player, platforms);
   this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.overlap(player, bombs, (player, bomb) => {
+    bomb.disableBody(true, true);
+    score -= 20;
+    scoreText.setText(`Score: ${score}`);
+  }, null, this)
+
 }
 
 function collectStar(player, star) {
-  console.log('remove')
   star.disableBody(true, true);
+  score += 10;
+  scoreText.setText('Score: ' + score);
 }
 
 function update() {
+  // bombs.map(bomb => hanldingEatBomb(bomb));
+  player.x == 0 && player.y == 0 ? console.log('Overlap') : null;
+
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
     player.anims.play('left', true);
@@ -100,6 +129,14 @@ function update() {
   }
 
   cursors.up.isDown ? player.setVelocityY(-330) : '';
+}
+
+function hanldingEatBomb(bomb) {
+  if (player.x == bomb.x && player.y === bomb.y) {
+    console.log('Hel')
+    score -= 20;
+    scoreText.setText(`Score: ${score}`);
+  }
 }
 
 function tryd() { console.log(this) }
